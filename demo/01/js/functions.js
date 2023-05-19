cda274dfe50e892ed60b591bd416809ae5c00941
lightingbro46 <quanganh10031996@gmail.com>
2023-05-20 02:54:22 +0700

    version 1.0

--- demo/01/js/functions.js
+++ demo/01/js/functions.js
@@ -18,26 +18,48 @@ $(window).resize(function () {
 (function ($) {
 	$.fn.typewriter = function (options) {
 		var settings = $.extend({
-			delay_break: []
+			timeline: []
 		}, options);
-
 		this.each(function () {
-			var $ele = $(this), str = $ele.html(), progress = 0;
+			var $ele = $(this), str = $ele.html(), position = 0;
+			var line_no = 0, break_line = 0, pre_space = false, process = 0;
+			var player = $("#player")[0];
 			$ele.html('');
 			var timer = setInterval(function () {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
+				var cur_t = player.currentTime;
+				process++;
+				if (cur_t < settings.timeline[line_no]) {
+					var substr = str.substring(0, position);
+					$ele.html(substr + ((process & 1) && (position < str.length) ? '_' : ''));
 				} else {
-					progress++;.
+					var current = str.substr(position, 1);
+					while (current == ' ') {
+						if (!pre_space) {
+							pre_space = true;
+							break;
+						}
+						position++;
+						current = str.substr(position, 1);
+					}
+					if (current != ' ') {
+						pre_space = false;
+					}
+					if (current == '<') {
+						position = str.indexOf('>', position) + 1;
+					} else {
+						position++;
+					}
+					var substr = str.substring(0, position);
+					if (substr.endsWith('<br>')) {
+						break_line++;
+						if (break_line == 2) {
+							line_no++;
+							break_line = 0;
+						}
+					}
+					$ele.html(substr + ((process & 1) && (position < str.length) ? '_' : ''));
 				}
-				var substr = str.substring(0, progress);
-				console.log(substr);
-				console.log("slice", str.slice(- 5));
-				console.log(substr.endsWith('<br>'));
-
-				$ele.html(substr + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
+				if (position >= str.length) {
 					clearInterval(timer);
 				}
 			}, 60);
@@ -69,48 +91,63 @@ function timeElapse(date) {
 }
 
 function lyricsElapse(time) {
-	console.log(time);
-	if (time > 14 && time < 49) {
+	if (time > 15 && time < 49) {
 		if (!$("#code-1").is(':visible')) {
-			console.log("show code-1");
-			$("#code-1").show().typewriter();
+			$("#code-2").hide();
+			$("#code-3").hide();
+			$("#code-4").hide();
+			$("#code-1").show().typewriter({
+				timeline: [16, 21, 25, 29, 33, 37, 42, 46]
+			});
 		}
 	}
 
 	if (time >= 49 && time < 99) {
 		if (!$("#code-2").is(':visible')) {
 			$("#code-1").hide();
-			console.log("show code-2")
-			$("#code-2").show().typewriter();
+			$("#code-3").hide();
+			$("#code-4").hide();
+			$("#code-2").show().typewriter({
+				timeline: [50, 55, 59, 63, 67, 71, 76, 80]
+			});
 		}
 	}
 
 	if (time >= 99 && time < 134) {
 		if (!$("#code-3").is(':visible')) {
+			$("#code-1").hide();
 			$("#code-2").hide();
-			console.log("show code-3")
-			$("#code-3").show().typewriter();
+			$("#code-4").hide();
+			$("#code-3").show().typewriter({
+				timeline: [101, 105, 109, 113, 118, 122, 126, 130]
+			});
 		}
 	}
 
 	if (time >= 134 && time < 183) {
 		if (!$("#code-2").is(':visible')) {
+			$("#code-1").hide();
 			$("#code-3").hide();
-			console.log("show code-2")
-			$("#code-2").show().typewriter();
+			$("#code-4").hide();
+			$("#code-2").show().typewriter({
+				timeline: [134, 139, 143, 147, 151, 156, 160, 164]
+			});
 		}
 	}
 
 	if (time >= 183) {
 		if (!$("#code-4").is(':visible')) {
+			$("#code-1").hide();
 			$("#code-2").hide();
-			console.log("show code-4")
-			$("#code-4").show().typewriter();
+			$("#code-3").hide();
+			$("#code-4").show().typewriter({
+				timeline: [185, 189, 194, 198, 202, 206, 210, 216, 220]
+			});
 		}
-	}
+	} 
 
 	if (time >= 225) {
-		if (!$("#clock-box").is(':visible')) { 
+		if (!$("#clock-box").is(':visible')) {
 			$("#clock-box").fadeIn(500, function () {
 				$("#proposal").fadeIn(10000);
 			});
